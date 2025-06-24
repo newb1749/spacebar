dce3781a55c041d6a92a71d24b4cad98ccda7036
newb1749 <newb1749@gmail.com>
2025-06-24 09:01:31 +0900

    feature: 카카오맵 시작

---
+++ src/main/java/com/sist/web/controller/MapController.java
@@ -0,0 +1,25 @@
+package com.sist.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller("mapController")
+public class MapController {
+	
+	private static Logger logger = LoggerFactory.getLogger(RoomController.class);
+		
+	@Value("#{env['kakao.map.js-key']}")
+	private String KAKAO_MAP_KEY;
+	
+	@RequestMapping(value="/component/mapModule", method=RequestMethod.GET)
+	public String mapModule(Model model) 
+	{	
+	    model.addAttribute("kakaoMapKey", KAKAO_MAP_KEY);
+		return "/component/mapModule";
+	}
+}
