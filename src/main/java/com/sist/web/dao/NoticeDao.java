e6bcdb4938f0734e97499302b774512f1b7ea22f
i.m_delay <delay233@naver.com>
2025-07-15 10:58:29 +0900

    sssㅇ

---
+++ src/main/java/com/sist/web/dao/NoticeDao.java
@@ -0,0 +1,18 @@
+package com.sist.web.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.sist.web.model.Notice;
+import com.sist.web.model.NoticeReply;
+
+@Repository
+public interface NoticeDao 
+{
+	List<Notice> selectNoticeList();
+    Notice selectNoticeById(int noticeSeq);
+    void insertNotice(Notice notice);
+    void insertReply(NoticeReply reply);
+    List<NoticeReply> selectRepliesByNotice(int noticeSeq);
+}
---
+++ src/main/java/com/sist/web/model/NoticeReply.java
@@ -0,0 +1,44 @@
+package com.sist.web.model;
+
+import java.util.Date;
+
+public class NoticeReply {
+    private int replySeq;
+    private int noticeSeq;
+    private String userId;
+    private String replyContent;
+    private Date regDt;
+    
+    // Getters and Setters
+	public int getReplySeq() {
+		return replySeq;
+	}
+	public void setReplySeq(int replySeq) {
+		this.replySeq = replySeq;
+	}
+	public int getNoticeSeq() {
+		return noticeSeq;
+	}
+	public void setNoticeSeq(int noticeSeq) {
+		this.noticeSeq = noticeSeq;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getReplyContent() {
+		return replyContent;
+	}
+	public void setReplyContent(String replyContent) {
+		this.replyContent = replyContent;
+	}
+	public Date getRegDt() {
+		return regDt;
+	}
+	public void setRegDt(Date regDt) {
+		this.regDt = regDt;
+	}
+    
+}
