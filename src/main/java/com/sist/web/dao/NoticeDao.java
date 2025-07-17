6d6d115b9ea567dcfda6cb0d73d325ea05d45010
Sirocheeze <tjdgur0804@naver.com>
2025-07-17 17:43:57 +0900

    룸디테일 마무리중

--- src/main/java/com/sist/web/dao/NoticeDao.java
+++ src/main/java/com/sist/web/dao/NoticeDao.java
@@ -1,22 +1,22 @@
-//package com.sist.web.dao;
-//
-//import java.util.List;
-//
-//import org.springframework.stereotype.Repository;
-//
-//import com.sist.web.model.Notice;
-//import com.sist.web.model.NoticeReply;
-//
-//@Repository
-//public interface NoticeDao 
-//{
-//	List<Notice> selectNoticeList();
-//	
-//    Notice selectNoticeById(int noticeSeq);
-//    
-//    void insertNotice(Notice notice);
-//    
-//    void insertReply(NoticeReply reply);
-//    
-//    List<NoticeReply> selectRepliesByNotice(int noticeSeq);
-//}
+package com.sist.web.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.sist.web.model.Notice;
+import com.sist.web.model.NoticeReply;
+
+@Repository
+public interface NoticeDao 
+{
+	List<Notice> selectNoticeList();
+	
+    Notice selectNoticeById(int noticeSeq);
+    
+    void insertNotice(Notice notice);
+    
+    void insertReply(NoticeReply reply);
+    
+    List<NoticeReply> selectRepliesByNotice(int noticeSeq);
+}
